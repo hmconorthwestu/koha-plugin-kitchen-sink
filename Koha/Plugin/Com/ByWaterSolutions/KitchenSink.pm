7e003fa6cd12cbf696848a5b44ca40ca635181c4
hmconorthwestu <hannah.co@northwestu.edu>
2020-04-16 09:46:31 -0700

    add -> search to Koha::Schema::Result::Collection

--- Koha/Plugin/Com/ByWaterSolutions/KitchenSink.pm
+++ Koha/Plugin/Com/ByWaterSolutions/KitchenSink.pm
@@ -163,7 +163,7 @@ sub report_step1 {
 
     my @libraries = Koha::Libraries->search;
     my @categories = Koha::Patron::Categories->search_limited({}, {order_by => ['description']});
-	my @collections = Koha::Schema::Result::Collection;
+	my @collections = Koha::Schema::Result::Collection->search;
     $template->param(
         libraries => \@libraries,
         collections => \@collections,
