193ebd78d78038464266d7d9061ad2b911ff510c
hmconorthwestu <hannah.co@northwestu.edu>
2020-10-15 15:30:31 -0700

    change file names and directory names and version to v0.0.80 in Weeding.pm

--- gulpfile.js
+++ gulpfile.js
@@ -13,20 +13,20 @@ const today = dt.format('Y-m-d');
 const package_json = JSON.parse(fs.readFileSync('./package.json'));
 const release_filename = `${package_json.name}-v${package_json.version}.kpz`;
 
-const pm_name = 'KitchenSink';
+const pm_name = 'Weeding';
 const pm_file = pm_name+'.pm';
-const pm_file_path = path.join('Koha', 'Plugin', 'Com', 'ByWaterSolutions');
+const pm_file_path = path.join('Koha', 'Plugin', 'Com', 'NULibrary');
 const pm_file_path_full = path.join(pm_file_path, pm_file);
 const pm_file_path_dist = path.join('dist', pm_file_path);
 const pm_file_path_full_dist = path.join(pm_file_path_dist, pm_file);
 const pm_bundle_path = path.join(pm_file_path, pm_name);
 
 /**
- * 
+ *
  * Array of directories relative to pm_bundle_path where static files will be served
- * 
+ *
  * If no static files need to be served, set static_relative_path = []
- * 
+ *
  */
 const static_relative_path = ['static_files', 'datepicker'];
 
